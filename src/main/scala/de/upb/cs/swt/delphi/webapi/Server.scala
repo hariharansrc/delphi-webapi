de1e1631d93dc32ad0ad0f9ced34f2f6ca127aeb
Ben Hermann <ben.hermann@googlemail.com>
2018-02-21 23:29:03 +0100

    Made the implementation easier

--- src/main/scala/de/upb/cs/swt/delphi/webapi/Server.scala
+++ src/main/scala/de/upb/cs/swt/delphi/webapi/Server.scala
@@ -1,37 +1,18 @@
 package de.upb.cs.swt.delphi.webapi
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
-
-import scala.io.StdIn
+import akka.http.scaladsl.server.HttpApp
 
 /**
   * Web server configuration for Delphi web API.
   */
-object Server {
+object Server extends HttpApp {
 
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-    implicit val executionContext = system.dispatcher
-
-    val route =
+   override def routes =
       path("version") { version } ~
         path("features") { features } ~
         pathPrefix("search" / Remaining) { query => search(query) } ~
         pathPrefix("retrieve" / Remaining) { identifier => retrieve(identifier) }
 
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
-  }
 
   private def version = {
     get {
@@ -63,5 +44,11 @@ object Server {
     }
   }
 
+  def main(args: Array[String]): Unit = {
+    Server.startServer("0.0.0.0", 8080)
+  }
+
 
 }
+
+
