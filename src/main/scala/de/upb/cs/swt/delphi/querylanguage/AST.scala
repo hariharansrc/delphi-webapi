f014bf0a8fd50f482e0a050b54b5f66c121926bb
Ben Hermann <ben.hermann@googlemail.com>
2018-10-22 11:06:06 +0200

    Renamed fields to conform to coding style

--- src/main/scala/de/upb/cs/swt/delphi/querylanguage/AST.scala
+++ src/main/scala/de/upb/cs/swt/delphi/querylanguage/AST.scala
@@ -18,18 +18,18 @@ package de.upb.cs.swt.delphi.querylanguage
 
 trait CombinatorialExpr
 
-case class AndExpr(Left: CombinatorialExpr, Right: CombinatorialExpr) extends CombinatorialExpr
-case class OrExpr(Left: CombinatorialExpr, Right: CombinatorialExpr) extends CombinatorialExpr
-case class NotExpr(Expr: CombinatorialExpr) extends CombinatorialExpr
-case class XorExpr(Left: CombinatorialExpr, Right: CombinatorialExpr) extends CombinatorialExpr
+case class AndExpr(left: CombinatorialExpr, right: CombinatorialExpr) extends CombinatorialExpr
+case class OrExpr(left: CombinatorialExpr, right: CombinatorialExpr) extends CombinatorialExpr
+case class NotExpr(expr: CombinatorialExpr) extends CombinatorialExpr
+case class XorExpr(left: CombinatorialExpr, right: CombinatorialExpr) extends CombinatorialExpr
 
 trait SingularConditionExpr extends CombinatorialExpr
 
-case class EqualExpr(Left: String, Right: String) extends SingularConditionExpr
-case class NotEqualExpr(Left: String, Right: String) extends SingularConditionExpr
-case class GreaterThanExpr(Left: String, Right: String) extends SingularConditionExpr
-case class GreaterOrEqualExpr(Left: String, Right: String) extends SingularConditionExpr
-case class LessThanExpr(Left: String, Right: String) extends SingularConditionExpr
-case class LessOrEqualExpr(Left: String, Right: String) extends SingularConditionExpr
-case class LikeExpr(Left: String, Right: String) extends SingularConditionExpr
-case class TrueExpr(Expr: String) extends SingularConditionExpr
+case class EqualExpr(left: String, right: String) extends SingularConditionExpr
+case class NotEqualExpr(left: String, right: String) extends SingularConditionExpr
+case class GreaterThanExpr(left: String, right: String) extends SingularConditionExpr
+case class GreaterOrEqualExpr(left: String, right: String) extends SingularConditionExpr
+case class LessThanExpr(left: String, right: String) extends SingularConditionExpr
+case class LessOrEqualExpr(left: String, right: String) extends SingularConditionExpr
+case class LikeExpr(left: String, right: String) extends SingularConditionExpr
+case class TrueExpr(expr: String) extends SingularConditionExpr
